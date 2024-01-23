c5476a9c00a2613263ee1ce1b7c4881e61413f9e
Dan Woodard <danwoodard+gh@gmail.com>
2024-01-23 11:46:07 -0500

    SF changes

---
+++ .config/nvim/after/plugin/plenary.lua
@@ -0,0 +1,3 @@
+local filetype = require "plenary.filetype"
+
+filetype.add_file('apex_type')
---
+++ .config/nvim/data/plenary/filetypes/apex_type.lua
@@ -0,0 +1,8 @@
+return {
+  -- fix the issue that telescope previewer doesn't code highlight apex
+  extension = {
+    ['cls'] = 'apex',
+    ['apex'] = 'apex',
+    ['trigger'] = 'apex',
+  }
+}
