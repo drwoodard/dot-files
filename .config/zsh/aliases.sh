adc7d0ebc9981c02395b260f8faee4b9c81d664e
Dan Woodard <danwoodard+gh@gmail.com>
2024-03-20 22:21:27 -0400

    Starship and aliases updates.

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -10,19 +10,25 @@ if command_exists lsd; then
     alias ls='lsd'
 fi
 
-alias ll='ls -l'
-alias la='ls -al'
-alias lt='ls --tree'
-
 if command_exists nvim; then
     alias vi="nvim"
     alias vim="nvim"
 fi
 
 if command_exists bat; then
-  alias cat='bat'
+    alias cat='bat'
 fi
 
-#Google Cloud
+# Google Cloud
 alias gssh="clear&gcloud cloud-shell ssh"
+
+alias ll='ls -l'
+alias la='ls -al'
+alias lt='ls --tree'
+
+alias s='source ~/.zshrc'
+
+if command_exists python3; then
+    alias serve='python3 -m http.server'
+fi
 
