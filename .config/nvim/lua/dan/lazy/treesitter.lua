cf8d88da4d460418ba77f0cb0f7d648de4ec37e0
Dan Woodard <danwoodard+gh@gmail.com>
2024-10-12 22:31:05 -0400

    Fixed continual treesitter updating.

--- .config/nvim/lua/dan/lazy/treesitter.lua
+++ .config/nvim/lua/dan/lazy/treesitter.lua
@@ -1,39 +1,22 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+    build = function()
+        -- Only run TSUpdate if parsers need updating or are missing
+        require('nvim-treesitter.install').update({ with_sync = true })
+    end,
     config = function()
         require("nvim-treesitter.configs").setup({
-            -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "c", "lua", "rust",
                 "jsdoc", "bash", "go", "yaml", "json", "query", "html", "css",
             },
-
-            --
-            --
-            --
-            --
-            --
-            --
-            -- Install parsers synchronously (only applied to `ensure_installed`)
             sync_install = false,
-
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
             auto_install = true,
-
             indent = {
                 enable = true
             },
-
             highlight = {
-                -- `false` will disable the whole extension
                 enable = true,
-
-                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-                -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
-                -- Using this option may slow down your editor, and you may see some duplicate highlights.
-                -- Instead of true it can also be a list of languages
                 additional_vim_regex_highlighting = { "markdown" },
             },
         })
@@ -42,7 +25,7 @@ return {
         treesitter_parser_config.templ = {
             install_info = {
                 url = "https://github.com/vrischmann/tree-sitter-templ.git",
-                files = {"src/parser.c", "src/scanner.c"},
+                files = { "src/parser.c", "src/scanner.c" },
                 branch = "master",
             },
         }
@@ -50,3 +33,5 @@ return {
         vim.treesitter.language.register("templ", "templ")
     end
 }
+
+
