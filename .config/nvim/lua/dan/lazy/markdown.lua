6ff39b31432109e3d4656a6ff354a37ff40ffd7e
Dan Woodard <danwoodard+gh@gmail.com>
2024-10-12 22:11:47 -0400

    Made markdown install async with schedule.

--- .config/nvim/lua/dan/lazy/markdown.lua
+++ .config/nvim/lua/dan/lazy/markdown.lua
@@ -1,6 +1,14 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function()
+    -- Use vim.schedule to run install after ensuring Neovim is ready
+    vim.schedule(function()
+      if not vim.fn["mkdp#util#installed"]() then
+        vim.fn["mkdp#util#install"]()
+      end
+    end)
+  end,
 }
+
