213d5cd596a0b0fd76240ded74951fa707a8baa6
Dan Woodard <danwoodard+gh@gmail.com>
2024-10-19 14:56:12 -0400

    Fixed telescope.

--- .config/nvim/lua/dan/lazy/telescope.lua
+++ .config/nvim/lua/dan/lazy/telescope.lua
@@ -11,12 +11,10 @@ return {
     require('telescope').setup({})
 
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    end)
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
   end
 }
+
